90f2e409172155016d189e896f1fc4e6ca05fe06
Hemal <hemalkathiriyauk@gmail.com>
2024-05-05 14:13:00 +0100

    Initial Cutscene Completed
    Only thing remaining is the Win/Lose Screen.

--- CrazyCritterProject/Assets/Cutscene.cs
+++ CrazyCritterProject/Assets/Cutscene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CutScene : MonoBehaviour
 {
@@ -9,12 +10,19 @@ public class CutScene : MonoBehaviour
 
     public GameObject CutSceneCluster;
 
+
     [SerializeField] GameObject HUD;
-    [SerializeField] EnemyAI enemyAI;
+
+    [SerializeField] NavMeshPatrol enemyPatrol;
+    [SerializeField] NavMeshAgent enemyAgent;
     [SerializeField] SmoothControl smoothControl;
 
+    [SerializeField] ColorBlindDropdown colorBlindDropdown;
     private void Awake()
     {
+        
+        //colorBlindDropdown.MenuValue = DataBank.Instance.MyStats.ColorBlindEnum;
+
         if (DataBank.Instance.MyStats.CutSceneDisplayed == true)
         {
             Destroy(CutSceneCluster);
@@ -24,50 +32,16 @@ public class CutScene : MonoBehaviour
     {
         smoothControl.enabled = false;
         HUD.SetActive(false);
-        enemyAI.enabled = false;
+
+        enemyPatrol.enabled = false;
+        enemyAgent.enabled = false;
+
+
         StartCoroutine(TeleportLerpRoutine());
     }
 
     private IEnumerator TeleportLerpRoutine()
     {
-        /*
-        transform.position = Teleports[1].position;
-        float elapsedTime = 0;
-        while (elapsedTime < lerpDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(Teleports[1].position, Lerps[1].position, elapsedTime / lerpDuration);
-            yield return null;
-        }
-
-        transform.position = Teleports[2].position;
-        elapsedTime = 0;
-        while (elapsedTime < lerpDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(Teleports[2].position, Lerps[2].position, elapsedTime / lerpDuration);
-            yield return null;
-        }
-
-        transform.position = Teleports[3].position;
-        elapsedTime = 0;
-        while (elapsedTime < lerpDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(Teleports[3].position, Lerps[3].position, elapsedTime / lerpDuration);
-            yield return null;
-        }
-        transform.position = Teleports[4].position;
-        elapsedTime = 0;
-        while (elapsedTime < lerpDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(Teleports[4].position, Lerps[4].position, elapsedTime / lerpDuration);
-            yield return null;
-        }
-        */
-        
-
         for (int i = 0; i < Teleports.Length; i++)
         {
             transform.position = Teleports[i].position;
@@ -85,7 +59,9 @@ public class CutScene : MonoBehaviour
         smoothControl.enabled = true;
 
         HUD.SetActive(true);
-        enemyAI.enabled = true;
+
+        enemyAgent.enabled = true;
+        enemyPatrol.enabled = true;
 
         DataBank.Instance.MyStats.CutSceneDisplayed = true;
         Destroy(CutSceneCluster);
